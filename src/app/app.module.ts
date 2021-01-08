b09ef3336f2d0a6fa89e0cf79d482efe58104f13
Hino-D-Bit <antonio.tamburello.dev@gmail.com>
2021-01-08 11:45:57 +0100

    Fix error import infobox (?)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { InfoboxComponent } from './infobox/infobox.component';
-import { AppRoutingModule } from '../app-routing.module';
-import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { AppRoutingModule } from "../app-routing.module";
+import { RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { InfoboxComponent } from "./home/infobox/infobox.component";
+import { CardComponent } from "./home/card/card.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule , AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, InfoboxComponent, HomeComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, RouterModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    InfoboxComponent,
+    HomeComponent,
+    CardComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
